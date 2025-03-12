3d77230dde2e15ea795a4f98f76cda6dac5b4429
mylenezijlstra <mylenezijlstra@icloud.com>
2025-03-12 11:50:55 +0100

    js verplaatst van html doc naar js doc

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,4 +29,154 @@ function myFunction() {
   }
 }
 
+//js voor hamburgermenu
+
+function resetForm() {
+  document.getElementById("alertBijNaam").innerHTML = "";
+  document.getElementById("alertAchterNaam").innerHTML = "";
+  document.getElementById("alertBijEmail").innerHTML = "";
+  document.getElementById("alertBijhuisnmr").innerHTML = "";
+  document.getElementById("alertBijToev").innerHTML = "";
+  document.getElementById("alertBijPostcode").innerHTML = "";
+  document.getElementById("alertBijTelefoon").innerHTML = "";
+  document.getElementById("alertBijDatum").innerHTML = "";
+}
+
+function validateForm() {
+
+  function validateForm() {
+      // Haal de geselecteerde aanhef op
+      let aanhef = document.querySelector('input[name="aanhef"]:checked').value;
+
+      // Voeg de aanhef toe aan de querystring voor de resultpagina
+      let form = document.forms["mijnFormulier"];
+      form.action = "resultaat.html?aanhef=" + aanhef + "&" + new URLSearchParams(new FormData(form)).toString();
+
+      return true;  // Verzendt het formulier
+  }
+
+  console.log("validateForm");
+  ///---------
+  let voornaam = document.forms["mijnFormulier"]["voornaam"].value;
+  if (voornaam == "") {
+      //alert("Name must be filled out");
+      document.getElementById("alertBijNaam").innerHTML =
+          "Naam moet ingevuld worden.";
+      return false;
+  }
+
+  ///---------
+  let achternaam = document.forms["mijnFormulier"]["achternaam"].value;
+  if (achternaam == "") {
+      //alert("Name must be filled out");
+      document.getElementById("alertAchterNaam").innerHTML =
+          "Achternaam moet ingevuld worden.";
+      return false;
+  }
+
+  ///---------
+  let Huisnummer = document.forms["mijnFormulier"]["Huisnummer"].value;
+  if (Huisnummer == "") {
+      //alert("Name must be filled out");
+      document.getElementById("alertBijhuisnmr").innerHTML =
+          "Huisnummer moet ingevuld worden.";
+      return false;
+  }
+
+
+  ///---------
+  let toev = document.forms["mijnFormulier"]["toev"].value;
+  if (toev == "") {
+      //alert("Name must be filled out");
+      document.getElementById("alertBijToev").innerHTML =
+          "Toev. moet ingevuld worden.";
+      return false;
+      ///---------
+  }
+
+  ///---------
+  let postcode = document.forms["mijnFormulier"]["postcode"].value;
+  if (postcode == "") {
+      //alert("Name must be filled out");
+      document.getElementById("alertBijPostcode").innerHTML =
+          "Postcode moet ingevuld worden.";
+      return false;
+      ///---------
+  }
+
+  ///---------
+  let email = document.forms["mijnFormulier"]["email"].value;
+  if (email == "") {
+      //alert("Name must be filled out");
+      document.getElementById("alertBijEmail").innerHTML =
+          "Email moet ingevuld worden.";
+      return false;
+      ///---------
+  }
+
+  ///---------
+  let Telefoonnummer = document.forms["mijnFormulier"]["Telefoonnummer"].value;
+  if (Telefoonnummer == "") {
+      //alert("Name must be filled out");
+      document.getElementById("alertBijTelefoon").innerHTML =
+          "Telefoonnummer moet ingevuld worden.";
+      return false;
+      ///---------
+  }
+
+  ///---------
+  let Datum = document.forms["mijnFormulier"]["Datum"].value;
+  if (Datum == "") {
+      //alert("Name must be filled out");
+      document.getElementById("alertBijDatum").innerHTML =
+          "Datum moet ingevuld worden.";
+      return false;
+      ///---------
+  }
+  let checkbpxchecked = document.getElementById("checkbox1").checked;
+  if (checkbpxchecked == false) {
+      //alert("Name must be filled out");
+      document.getElementById("alertBijCheckbox").innerHTML =
+          "Algemene voorwaarden accepteren.";
+      return false;
+      ///---------
+  }
+
+
+}
+
+//js voor formulier
+
+ // Haal de queryparameters op uit de URL
+ let url = new URL(window.location.href);
+
+ let aanhef = url.searchParams.get("aanhef");
+ let voornaam = url.searchParams.get("voornaam");
+ let tussenv = url.searchParams.get("tussenv");
+ let achternaam = url.searchParams.get("achternaam");
+ let Huisnummer = url.searchParams.get("Huisnummer");
+ let toev = url.searchParams.get("Straatnaam");
+ let postcode = url.searchParams.get("Postcode");
+ let Telefoonnummer = url.searchParams.get("Telefoonnummer");
+ let email = url.searchParams.get("email");
+ let Datum = url.searchParams.get("Datum");
+ let land = url.searchParams.get("Land");
+
+ // Vul de inhoud van het element met ID "resultaat" in
+ document.getElementById("resultaat").innerHTML =
+
+     "Aanhef: " + aanhef +
+     "<br><br>Voornaam: " + voornaam +
+     "<br><br>Tussenvoegsel: " + tussenv +
+     "<br><br>Achternaam: " + achternaam +
+     "<br><br>Huisnummer: " + Huisnummer +
+     "<br><br>Straatnaam: " + toev +
+     "<br><br>Postcode: " + postcode +
+     "<br><br>Land: " + land +
+     "<br><br>Email: " + email +
+     "<br><br>Telefoonnummer: " + Telefoonnummer +
+     "<br><br>Geboortedatum: " + Datum;
+
+     //js voor resultaat
+
 
